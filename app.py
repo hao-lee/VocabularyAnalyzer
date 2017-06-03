3c83b29a71cc832ab61be7b26b2701e2f7dc8fce
Hao Lee <haolee.swjtu@gmail.com>
2017-06-03 15:58:48 +0800

    使用 gevent 处理异步请求
    通过 gevent 的 Monkey patching 给 Flask 增加异步请求处理功能
    
    Signed-off-by: Hao Lee <haolee.swjtu@gmail.com>

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+# 关于 gevent + Flask 的例子: https://github.com/pallets/flask/issues/1073
+from gevent import monkey
+monkey.patch_all()
 
+from gevent import wsgi
+from flask import Flask
 import sys
 sys.path.append("utils")
-from flask import Flask
 import interpreter
 import analyzer
 
@@ -15,4 +19,7 @@ app.register_blueprint(interpreter.bp_pti)
 app.register_blueprint(analyzer.bp_va)
 
 if __name__ == '__main__':
-	app.run()
+	#app.run()  # 单纯的 Flask app
+	# 使用 gevent 异步处理请求
+	server = wsgi.WSGIServer(('127.0.0.1', 5000), app)
+	server.serve_forever()
