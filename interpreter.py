120faa9f95255a6526b67a07e6b18acaea6c55c4
Hao Lee <haolee.swjtu@gmail.com>
2017-06-03 22:41:34 +0800

    音标生成器不再断句
    一次性将全文进行分词，不再进行断句
    
    Signed-off-by: Hao Lee <haolee.swjtu@gmail.com>

--- interpreter.py
+++ interpreter.py
@@ -28,10 +28,7 @@ def processing():
 	with open("data/words.txt") as fd:
 		ultra_word_list = fd.read().split()
 	text = request.form["text"]
-	# 断句分词
-	matrix = []  # 每行都是句子被分词之后产生的单词列表
-	for sentence in nlp.nltk_sentence_tokenizer(text):
-		matrix.append(nlp.nltk_word_tokenizer(sentence))
+	wordlist = nlp.nltk_word_tokenizer(text)
 	'''
 	生成参考字典，其格式如下：
 	refer_dict = '{
@@ -46,54 +43,48 @@ def processing():
 	wait_to_save = {}
 	db = database.DatabaseManager()
 	db.open()
-	for row in matrix: # 一行
-		for word in row: # 行中某个词
-			print("Current Word: %s" %word)
-			# 首先检测 word 是不是英语单词
-			if word.lower() not in ultra_word_list:
-				refer_dict[word.lower()] = {"pos_pron":[":"], "index":0}
-				print("%s 不是英语单词" %word)
-				continue
-			pos_pron_str = db.query(word.lower())
-			if pos_pron_str is not None:
-				# 数据库查出的是字符串，要转为list类型
-				pos_pron = json.loads(pos_pron_str)
-				print("Hit in DB cache.")
-			else:
-				pos_pron = cambridge_crawler.crawler(word.lower())
-				# 传给数据库时保证键值都是字符串类型
-				wait_to_save[word.lower()] = json.dumps(pos_pron)
-				print("Crawl from URL.")
-			# pos_pron 是一个list，存储了该单词每个词性的音标
-			refer_dict[word.lower()] = {"pos_pron":pos_pron, "index":0}
-	
+	for word in wordlist:
+		print("Current Word: %s" %word)
+		# 首先检测 word 是不是英语单词
+		if word.lower() not in ultra_word_list:
+			refer_dict[word.lower()] = {"pos_pron":[":"], "index":0}
+			print("%s 不是英语单词" %word)
+			continue
+		pos_pron_str = db.query(word.lower())
+		if pos_pron_str is not None:
+			# 数据库查出的是字符串，要转为list类型
+			pos_pron = json.loads(pos_pron_str)
+			print("Hit in DB cache.")
+		else:
+			pos_pron = cambridge_crawler.crawler(word.lower())
+			# 传给数据库时保证键值都是字符串类型
+			wait_to_save[word.lower()] = json.dumps(pos_pron)
+			print("Crawl from URL.")
+		# pos_pron 是一个list，存储了该单词每个词性的音标
+		refer_dict[word.lower()] = {"pos_pron":pos_pron, "index":0}
+
 	# 将新的数据保存到数据库
 	db.save_to_db(wait_to_save)
 	db.close()
 	
 	# 组装 HTML 片段
 	content_block = ""
-	text_wc = 0
-	for row in matrix: # 一行
-		# 行首加一格空的div，以便换行
-		content_block += "<div class=\"no-data\"></div>"		
-		for word in row: # 行中某个词
-			pos_pron_element = refer_dict[word.lower()]["pos_pron"][0]
-			pos = pos_pron_element.split(":")[0]
-			pron = pos_pron_element.split(":")[1]
-			content_block += "<div class=\"group\" title=\"%s\">\n"	\
-			                  "\t<p class=\"word\">%s</p>"		\
-			                  "\t<p class=\"pronunciation\">%s</p>"	\
-			                  "</div>\n"				\
-			                  %(pos, word, pron)
-			text_wc += 1
-		# 一行拼接完成
+	for word in wordlist:
+		pos_pron_element = refer_dict[word.lower()]["pos_pron"][0]
+		pos = pos_pron_element.split(":")[0]
+		pron = pos_pron_element.split(":")[1]
+		content_block += "<div class=\"group\" title=\"%s\">\n"	\
+		        "\t<p class=\"word\">%s</p>"		\
+		        "\t<p class=\"pronunciation\">%s</p>"	\
+		        "</div>\n"				\
+		        %(pos, word, pron)
 	# 循环完成后，HTML 片段生成完毕
 
 	# 字典转json字符串
 	refer_dict_str = json.dumps(refer_dict)
 
 	# print(refer_dict_str)
+	text_wc = len(wordlist)
 	end_time = time.time()  # 计时终点
 	elapsed_time = end_time-start_time
 	content_block = ("<h5>输入词汇数: %d 个</h5>" %text_wc) \
