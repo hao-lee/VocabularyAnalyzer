856a79d6858832854383bbe08fa61a92c0b0459b
Hao Lee <haolee.swjtu@gmail.com>
2017-06-03 18:52:35 +0800

    Phonetic Transcription Interpreter 增加计时功能并统计单词个数
    Signed-off-by: Hao Lee <haolee.swjtu@gmail.com>

--- interpreter.py
+++ interpreter.py
@@ -5,6 +5,7 @@ sys.path.append("utils")
 from flask import request, render_template, Blueprint
 import json
 import sqlite3
+import time
 import nlp
 import cambridge_crawler
 import database
@@ -22,12 +23,13 @@ def submit():
 # 处理数据并返回结果页面
 @bp_pti.route('/phonetic_transcription_interpreter', methods=['POST'])
 def processing():
+	start_time = time.time()  # 计时起点
 	# 读取用于拼写检查的超级单词表
 	with open("data/words.txt") as fd:
 		ultra_word_list = fd.read().split()
 	text = request.form["text"]
 	# 断句分词
-	matrix = []  # 每行都是句子被分词之后的
+	matrix = []  # 每行都是句子被分词之后产生的单词列表
 	for sentence in nlp.nltk_sentence_tokenizer(text):
 		matrix.append(nlp.nltk_word_tokenizer(sentence))
 	'''
@@ -71,6 +73,7 @@ def processing():
 	
 	# 组装 HTML 片段
 	content_block = ""
+	text_wc = 0
 	for row in matrix: # 一行
 		# 行首加一格空的div，以便换行
 		content_block += "<div class=\"no-data\"></div>"		
@@ -83,6 +86,7 @@ def processing():
 			                  "\t<p class=\"pronunciation\">%s</p>"	\
 			                  "</div>\n"				\
 			                  %(pos, word, pron)
+			text_wc += 1
 		# 一行拼接完成
 	# 循环完成后，HTML 片段生成完毕
 
@@ -90,6 +94,11 @@ def processing():
 	refer_dict_str = json.dumps(refer_dict)
 
 	# print(refer_dict_str)
+	end_time = time.time()  # 计时终点
+	elapsed_time = end_time-start_time
+	content_block = ("<h5>输入词汇数: %d</h5>" %text_wc) \
+		+ ("<h5>执行时间: %f</h5>" %elapsed_time) \
+	        + content_block
 	return render_template('pti_result.html',
 	                       refer_dict_str=refer_dict_str,
 	                       content_block=content_block)
