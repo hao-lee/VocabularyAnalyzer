8f02a05bb369eecb2a28dfcebcaf27725b91173e
Hao Lee <haolee.swjtu@gmail.com>
2017-07-09 16:29:46 +0800

    添加百度统计代码
    Signed-off-by: Hao Lee <haolee.swjtu@gmail.com>

---
+++ static/common.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?48948a10b00bbba5de892fc23c90bd72";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
