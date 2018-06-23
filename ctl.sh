ffe2e0e3947802b6888569d7c15872b71086a883
Hao Lee <haolee.swjtu@gmail.com>
2018-06-23 11:44:04 +0800

    Update ctl.sh for new domain
    Signed-off-by: Hao Lee <haolee.swjtu@gmail.com>

--- ctl.sh
+++ ctl.sh
@@ -6,7 +6,7 @@ fi
 if [ "$1"x = "start"x ]; then
 	NLTK_DATA="/var/nltk_data/" nohup python3 app.py > nohup.log 2>&1 &
 elif [ "$1"x = "stop"x ]; then
-	curl "http://tools.eflclub.me/shutdown"
+	curl "http://efl.haolee.io/shutdown"
 else
 	echo "Usage: ./ctl start/stop"
 fi
